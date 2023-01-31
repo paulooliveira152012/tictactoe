7e1692cf0eaab349de4a061655b947afca2552ed
Paulo Oliveira <83649589+paulooliveira152012@users.noreply.github.com>
2023-01-31 13:53:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,9 +6,37 @@ let lastClicked;
 
 console.log(box)
 
-//make each box selectable
+
+
+function firstPlayer() {
+    //make each box selectable
 box.forEach(item => {
+    turn = playerOne
     item.addEventListener("click", event => {
-        console.log("test")
+        //console.log("test")
+        console.log('last selected was ' + turn)
+        //add an X to a selected box
+        item.style.backgroundColor="blue";
+        item.innerText=playerOne;
+
+        secondPlayer()
     })
 })
+}
+
+function secondPlayer() {
+    console.log("now it is " + playerTwo + "'s turn")
+    box.forEach(item => {
+        turn = playerOne
+        item.addEventListener("click", event => {
+            //console.log("test")
+            console.log('last selected was ' + turn)
+            //add an X to a selected box
+            item.style.backgroundColor="blue";
+            item.innerText=playerTwo;
+            firstPlayer()
+        })
+    })
+}
+
+firstPlayer()
